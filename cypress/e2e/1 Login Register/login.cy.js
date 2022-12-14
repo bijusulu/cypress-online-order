cadf07f9a385fb2369cff1bbe8e9f7c6fbd2858f
Bijay Sulu <biju.sulu@gmail.com>
2022-12-14 10:20:50 +0545

    add login script on login folder

---
+++ cypress/e2e/1 Login Register/login.cy.js
@@ -0,0 +1,31 @@
+describe("Visit Indochen Dev Site", () => {
+    beforeEach(() => {
+        cy.viewport("macbook-15");
+    });
+    it("Visit Homepage Indochen Dev", () => {
+        cy.visit("indochen.appdiscoverylab.com");
+    });
+
+    it("Choose Location Cameron Station", () => {
+        cy.get(":nth-child(1) > .location > .name > .theme-btn").click();
+    });
+
+    it("Open Login Dialog Modal", () => {
+        // cy.visit("https://indochen.appdiscoverylab.com/cameron-station/");
+        cy.get(".login-register-links").click();
+        cy.get(".modal-dialog").should("be.visible");
+    });
+
+    it("require user email", () => {
+        cy.get(".login-email").type("adl.mytest@gmail.com");
+    });
+
+    it("require password", () => {
+        cy.get(".login-password").type("bijusulu");
+    });
+
+    it("click login", () => {
+        cy.contains("Log In").click();
+        cy.get(".modal-dialog").should("not.be.visible");
+    });
+});
